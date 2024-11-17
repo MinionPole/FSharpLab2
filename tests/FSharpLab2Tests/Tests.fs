2d19854bbb809c89e1d44a84bfda38e87c7ee603
Roman <roma.yunusov.2001@mail.ru>
2024-11-17 20:05:14 +0300

    update test

--- tests/FSharpLab2Tests/Tests.fs
+++ tests/FSharpLab2Tests/Tests.fs
@@ -8,3 +8,25 @@ open System.Collections.Generic
 
 [<Fact>]
 let ``My test`` () = Assert.True(true)
+
+[<Fact>]
+let ``Contain test`` () =
+    Assert.True(
+        AVLBag.empty
+        |> AVLBag.add 1
+        |> AVLBag.add 2
+        |> AVLBag.add 10
+        |> AVLBag.add 5
+        |> AVLBag.tryGet 5 = 1
+    )
+
+[<Fact>]
+let ``Multiple contain test`` () =
+    Assert.True(
+        AVLBag.empty
+        |> AVLBag.add 1
+        |> AVLBag.add 2
+        |> AVLBag.add 10
+        |> AVLBag.add 1
+        |> AVLBag.tryGet 1 = 2
+    )
